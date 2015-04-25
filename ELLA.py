fb41ad8273fd2d6374beaa7be03be9f2dbcad65a
paulruvolo <paullundyruvolo@gmail.com>
2015-04-25 17:32:30 -0400

    switch to be more scikit learn like

--- ELLA.py
+++ ELLA.py
@@ -7,19 +7,21 @@ import numpy as np
 from scipy.linalg import sqrtm, inv, norm
 from sklearn.linear_model import LinearRegression, Ridge, LogisticRegression, Lasso
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, explained_variance_score
 
 class ELLA(object):
     """ The ELLA model """
     def __init__(self,d,k,base_learner,mu=1,lam=1):
-	""" Initializes a new model for the given base_learner.
-	    d: the number of parameters for the base learner
-	    k: the number of latent model components
-	    base_learner: the base learner to use (currently can only be
-	    		  LinearRegression, Ridge, or LogisticRegression
-	    mu: the L_1 penalty to use
-	    lam: the L_2 penalty to use
-	    NOTE: currently we don't have a way to specify hyperparameters
-	"""
+    	""" Initializes a new model for the given base_learner.
+    	    d: the number of parameters for the base learner
+    	    k: the number of latent model components
+    	    base_learner: the base learner to use (currently can only be
+    	    		  LinearRegression, Ridge, or LogisticRegression).
+    	    mu: the L_1 penalty to use
+    	    lam: the L_2 penalty to use
+    	    NOTE: currently we don't have a way to specify hyperparameters
+            NOTE: currently only binary logistic regression is supported
+    	"""
         self.d = d
         self.k = k
         self.L = np.random.randn(d,k)
@@ -29,19 +31,24 @@ class ELLA(object):
         self.T = 0.0
         self.mu = mu
         self.lam = lam
-        if base_learner not in [LinearRegression,LogisticRegression,Ridge]:
+        if base_learner in [LinearRegression, Ridge]:
+            self.perf_metric = explained_variance_score
+        elif base_learner in [LogisticRegression]:
+            self.perf_metric = accuracy_score
+        else:
             raise Exception("Unsupported Base Learner")
+
         self.base_learner = base_learner
 
     def fit(self,X,y,task_id):
-	""" Fit the model to a new batch of training data.  The task_id must
-	    start at 0 and increase by one each time this function is called.
-	    Currently you cannot add new data to old tasks.
+        """ Fit the model to a new batch of training data.  The task_id must
+            start at 0 and increase by one each time this function is called.
+            Currently you cannot add new data to old tasks.
 
-	    X: the training data
-	    y: the trianing labels
-	    task_id: the id of the task
-	"""
+            X: the training data
+            y: the trianing labels
+            task_id: the id of the task
+        """
         self.T += 1
         single_task_model = self.base_learner(fit_intercept=False).fit(X,y)
         D_t = self.get_hessian(single_task_model, X, y)
@@ -59,27 +66,35 @@ class ELLA(object):
         self.revive_dead_components()
 
     def revive_dead_components(self):
-	""" re-initailizes any components that have decayed to 0 """
+        """ re-initailizes any components that have decayed to 0 """
         for i,val in enumerate(np.sum(self.L,axis=0)):
-            if val < 10**-8:
+            if abs(val) < 10**-8:
                 self.L[:,i] = np.random.randn(self.d,)
 
     def predict(self,X,task_id):
-	""" Output ELLA's predictions for the specified data on the specified
-	    task_id.  If using a continuous model (Ridge and LinearRegression)
-	    the result is the prediction.  If using a classification model
-	    (LogisticRgerssion) the output is currently a probability.
-	"""
+        """ Output ELLA's predictions for the specified data on the specified
+            task_id.  If using a continuous model (Ridge and LinearRegression)
+            the result is the prediction.  If using a classification model
+            (LogisticRgerssion) the output is currently a probability.
+        """
         if self.base_learner == LinearRegression or self.base_learner == Ridge:
             return X.dot(self.L.dot(self.S[:,task_id]))
         elif self.base_learner == LogisticRegression:
-            return 1./(1.0+np.exp(-X.dot(self.L.dot(self.S[:,task_id]))))
+            return 1./(1.0+np.exp(-X.dot(self.L.dot(self.S[:,task_id])))) > 0.5
+
+    def score(self,X,y,task_id):
+        """ Output the score for ELLA's model on the specified testing data.
+            If using a continuous model (Ridge and LinearRegression)
+            the score is explained variance.  If using a classification model
+            (LogisticRegression) the score is accuracy.
+        """
+        return self.perf_metric(self.predict(X, task_id),y)
 
     def get_hessian(self, model, X, y):
-	""" ELLA requires that each single task learner provide the Hessian
-	    of the loss function evaluated around the optimal single task
-	    parameters.  This funciton implements this for the base learners
-	    that are currently supported """
+        """ ELLA requires that each single task learner provide the Hessian
+            of the loss function evaluated around the optimal single task
+            parameters.  This funciton implements this for the base learners
+            that are currently supported """
         theta_t = model.coef_
         if self.base_learner == LinearRegression:
             return X.T.dot(X)/(2.0*X.shape[0])
